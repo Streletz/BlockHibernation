2b82ac40d485edbf46e643829b3b243ee54e14bf
Streletz <streletzcoder@yandex.ru>
2019-01-28 01:15:02 +0300

    Комментарий к классу

--- EngineUwp/IdleTimerManagerUwp.cs
+++ EngineUwp/IdleTimerManagerUwp.cs
@@ -5,6 +5,12 @@ using Windows.System.Display;
 
 namespace EngineUwp
 {
+    /// <summary>
+    /// Работа с таймером спящего режима для UWP.
+    /// Большая часть методов суть "заглушки" на случай, если в будущем удастся достичь
+    /// функциональности аналогичной импорту SetThreadExecutionState из kernel32.dll (под UWP он недоступен).
+    /// Если нет, код этого и базового класса планируется оптимизировать.
+    /// </summary>
     public class IdleTimerManagerUwp : IdleTimerManager
     {
         private DisplayRequest _displayRequest;
